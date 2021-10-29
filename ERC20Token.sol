ed7fdbf0fd893235fd8361e6b98b7be33f65ffad
Sanjaya Panigrahi <47269667+sanjaya-panigrahi@users.noreply.github.com>
2021-10-29 13:19:06 +0530

    Create ERC20Token.sol

---
+++ ERC20Token.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.9.0;
+
+
+contract ERC20Token {
+
+// As per the EIP20 documentation these are some of the funcations we need to implement for a ERC 20 Standard
+  string public name = "XYZ"; 
+  string public symbol = "XYZ";
+  uint256 public decimals = 18;
+  uint256 public totalSupply;
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {}
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {}
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+}
